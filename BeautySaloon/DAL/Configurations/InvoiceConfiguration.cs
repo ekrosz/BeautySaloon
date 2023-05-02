acbc4c4d84f486340397036679d3ae731b775271
Мари Шмелькова <Shmelkova_Marina163@mail.ru>
2023-05-02 20:56:59 +0400

    Реализована entity и настроена конфигурация сущности

---
+++ BeautySaloon/DAL/Configurations/InvoiceConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using BeautySaloon.DAL.Configurations.Abstract;
+using BeautySaloon.DAL.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySaloon.DAL.Configurations
+{
+    public class InvoiceConfiguration : EntityConfiguration<Invoice>
+    {
+        public override void Configure(EntityTypeBuilder<Invoice> builder)
+        {
+
+            builder.Property(x => x.InvoiceType)
+            .IsRequired();
+
+            builder.Property(x => x.Count)
+            .IsRequired();
+
+            builder.Property(x => x.Cost)
+            .IsRequired(false);
+
+            builder.Property(x => x.Comment)
+            .HasMaxLength(500)
+            .IsRequired(false);
+
+            builder.HasOne(x => x.Employee)
+                .WithMany()
+                .HasForeignKey("UserId")
+                .IsRequired(false);
+
+            builder.HasOne(x => x.Material)
+               .WithMany()
+               .HasForeignKey("MaterialId")
+               .IsRequired();
+
+            base.Configure(builder);
+        }
+    }
+}
---
+++ BeautySaloon/DAL/Entities/Invoice.cs
@@ -0,0 +1,49 @@
+﻿using BeautySaloon.DAL.Entities.Contracts;
+using BeautySaloon.DAL.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySaloon.DAL.Entities
+{
+    public class Invoice : IEntity, IAuditable
+    {
+        public Guid Id { get; set; }
+
+        public InvoiceType InvoiceType { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime UpdatedOn { get; set; }
+
+        public User? Employee { get; set; } = default!;
+
+        public string? Comment { get; set; }
+
+        public int Count { get; set; }
+
+        public decimal? Cost { get; set; }
+
+        public Material Material { get; set; } = default!;
+
+        public Guid UserModifierId { get; set; }
+
+        public void Update(
+        InvoiceType invoiceType,
+        int count,
+        decimal cost,
+        Material material,
+        User? employee,
+        string? comment)
+        {
+            InvoiceType = invoiceType;
+            Count = count;
+            Cost = cost;
+            Material = material;
+            Employee = employee;
+            Comment = comment;
+        }
+    }
+}
